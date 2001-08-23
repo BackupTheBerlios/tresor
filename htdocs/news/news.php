23c31c99398370c11a75f468d00a8c102ca5dc2a
tbonk <tbonk>
2001-08-23 21:58:52 +0000

    .

---
+++ htdocs/news/news.php
@@ -0,0 +1,6 @@
+<ul>
+  <li>
+    <b>23.08.2001:</b> I decided to implem./Projekte/Tresor/htdocs/newsent Tresor in C++ and Qt. By using Qt, it'll be
+    possible to create version for MS Windows and MacOS. And besides, it'll be faster ;-)
+  </li>
+</ul>
