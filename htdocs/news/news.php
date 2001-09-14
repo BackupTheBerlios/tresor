881c32569950cbf98b3ac2d21d2802c9224facc9
tbonk <tbonk>
2001-09-14 16:36:37 +0000

    .

--- htdocs/news/news.php
+++ htdocs/news/news.php
@@ -1,7 +1,12 @@
 <ul>
   <li>
+    <b>14.09.2001:</b> The project is still alive! I'm working on the C++/Qt based version.
+    Development isn't that fast since I finished my studies last week and started my first
+    employment. I'll try to release a first version in November. So stay tuned!
+  </li>
+  <li>
     <b>23.08.2001:</b> I decided to implement Tresor in C++ and Qt. By using Qt, it'll be
-    possible to create version for MS Windows and MacOS. And besides, it'll be faster ;-)
+    possible to create version for Linux, MS Windows and MacOS. And besides, it'll be faster ;-)
     <br>
     The Java sourcecode is available from CVS. Maybe it is useful for somebody...
   </li>
